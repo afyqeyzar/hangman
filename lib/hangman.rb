e57ece52d9b0a2c6b39e95ba90f96ee3e222a12b
afyqeyzar <norafyqe@gmail.com>
2022-08-19 23:40:04 +0100

    changes the methods into a class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,21 +1,35 @@
 words = File.read('google-10000-english-no-swears.txt').split
 
-def four_letter_words(words)
-	new_words = words.map{|word| word unless word.length < 4}.uniq
-	new_words.delete_at(0)
-	new_words
+class GetWord
+	def initialize(words)
+		@words = words
+	end
+
+	def four_letter_words
+		@new_words = @words.map{|word| word unless word.length < 4}.uniq
+		@new_words.delete_at(0)
+		@new_words
+	end
+
+	def get_random_word
+		self.four_letter_words
+		@length = @new_words.length
+		@selected_word = @new_words[rand(0..@length)]
+	end
+
+	def selected_word
+		self.get_random_word
+		puts @selected_word
+		@display_word = Array.new(@selected_word.length,"_")
+		#p @display_word
+	end
+
+	def display
+		puts @display_word.join("")
+	end
 end
 
-def get_random_word(words)
-  length = four_letter_words(words).length
-  four_letter_words(words)[rand(0..length)]
-end
-
-def display_word(words)
-	selected_word = get_random_word(words)
-	#puts selected_word
-	display_word = Array.new(selected_word.length,"_")
-	puts display_word.join("")
-end
-
-display_word(words)
+game = GetWord.new(words)
+#game.get_random_word
+game.selected_word
+game.display
